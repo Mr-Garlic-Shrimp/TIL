e0b04425069311df2909554979e4b981714ec948
Mr-Garlic-Shrimp <pometango50.19@gmail.com>
2022-08-28 13:17:58 +0000

    CTデータとマスクの重ね合わせについて学習

--- Python/advanced/visualization_CTdata/.ipynb_checkpoints/ctvis-checkpoint.py
+++ Python/advanced/visualization_CTdata/.ipynb_checkpoints/ctvis-checkpoint.py
@@ -38,7 +38,7 @@ def label_color(mask_volume,
     カラーラベルを設定した4次元配列を返す。
     '''
     shp = mask_volume.shape
-    # 輝度値を含んだ4次元配列を作成
+    # 輝度値のを含んだ4次元ゼロ配列を作成
     mask_color = np.zeros((shp[0], shp[1], shp[2], 3),dtype=np.float32)
 
     # 色付け。mask_volumeの各要素とラベル番号が等しい箇所へカラーラベルのリストを代入
@@ -47,3 +47,36 @@ def label_color(mask_volume,
     mask_color[np.equal(mask_volume, 3)] = effusion_color
     
     return mask_color
+
+
+def hu_to_gray(volume):
+    '''
+    CTデータを受け取り、HUを0-255に変換する。
+    変換後、マスクデータと同じshapeに揃えた配列を返す。
+    '''
+    
+    maxhu = np.max(volume)
+    minhu = np.min(volume)
+    # 値を0-255に変換する
+    volume_rerange = (volume - minhu) / max((maxhu - minhu), 1e-3)
+    volume_rerange = volume_rerange * 255
+    
+    # マスクと重ねるために、volume_rerangeのshapeを合わせる
+    volume_rerange = np.stack([volume_rerange, volume_rerange, volume_rerange], axis=-1)
+    
+    # 0-255のintにしたいので、下記のようにuint8として返す
+    return volume_rerange.astype(np.uint8)
+
+
+def overlay(gray_volume, mask_volume, mask_color, alpha):
+    '''
+    グレイスケールのCTデータ、マスクデータ、カラーのマスクデータを受け取り、
+    CTデータとマスクデータを重ね合わせた配列を返す。
+    '''
+    mask_filter = np.greater(mask_volume, 0)
+    mask_filter = np.stack([mask_filter, mask_filter, mask_filter], axis=-1)
+    overlayed = np.where(mask_filter, 
+                         ((1-alpha)*gray_volume + alpha*mask_color).astype(np.uint8),
+                         gray_volume)
+    
+    return overlayed
