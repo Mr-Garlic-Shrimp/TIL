52bea01692e8fc1f1ca0eb047f3e69e191a3dbba
Mr-Garlic-Shrimp <pometango50.19@gmail.com>
2022-10-05 23:04:34 +0000

    最急降下法におけるMSEの算出を実装

---
+++ Machine Learning/.ipynb_checkpoints/cost_func-checkpoint.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# sampleデータはnumpyで渡す？それともdf？
+def cost_func(theta, target_var, features):
+    """
+    args:
+        theta: array like.parameter of regression
+        target_var: array like.target variable
+        features: array like.feature value.
+    return value of cost function
+    """
+    # サンプルデータ数を定義
+    m = len(target_var)
+    # MSE（残差の二乗和の平均）の計算
+    mse = np.power( (target_var - (theta[0] + theta[1] * features)) , 2 ) / m
+    return np.sum(mse)
+    
+    
