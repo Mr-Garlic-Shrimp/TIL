f303f32945014b800fe52f171995b8c9c8f6cecc
Mr-Garlic-Shrimp <pometango50.19@gmail.com>
2022-10-06 23:49:43 +0000

    最急降下法を配列を使って実装

--- Machine Learning/cost_func.py
+++ Machine Learning/cost_func.py
@@ -1,20 +1,35 @@
 import numpy as np
 
-# sampleデータはnumpyで渡す？それともdf？
+# インターフェースは2次元配列で統一する。
 def cost_func(theta, target_var, features):
     """
     args:
         theta: array like.parameter of regression
-        target_var: array like.target variable
-        features: array like.feature value.
+        target_var: 1Darray.target variable
+        features: NDarray.feature values.
     return value of cost function
     """
     # サンプルデータ数を定義
     m = len(target_var)
+    # target_varをm行1列にreshape
+    target_var_matrix = target_var.reshape(m, 1)    
+    
+    # featuresが1次元配列であれば、1行m列の2次元配列にする
+    # そうでなければ転置する
+    if features.ndim == 1:
+        features = features.reshape(1, m)
+    else:
+        features = features.T
+        
     # 1行m列の単位行列を定義
     ones  = np.ones((1, m))
-    # MSE（残差の二乗和の平均）の計算
-    mse = np.power( (target_var - (theta[0] + theta[1] * features)) , 2 ) / m
-    return np.sum(mse)
+    # 特徴量の配列の1行目にonesを結合
+    ones_features = np.concatenate([ones,features])
+    
+    # 残差の二乗の計算
+    sq_errors = (target_var_matrix - np.dot(ones_features.T, theta.T)) ** 2
+    # mseの算出
+    mse = np.mean(sq_errors)
+    return mse
     
     
