f007ef0f8d01c556ab7cbd8198cd7c24ecca9ca5
Mr-Garlic-Shrimp <pometango50.19@gmail.com>
2022-10-08 06:07:49 +0000

    損失関数を可視化

--- Machine Learning/.ipynb_checkpoints/cost_func-checkpoint.py
+++ Machine Learning/.ipynb_checkpoints/cost_func-checkpoint.py
@@ -1,20 +1,30 @@
+# 変更履歴
+# 10/7 実装。
+# 10/8 thetaをリストで渡せるようにcost_funcを修正した。
+# ---------------------------------------------------
+
 import numpy as np
+from itertools import product
 
 def cost_func(theta, target_var, features):
     """
     args:
-        theta: array like.parameter of regression
-        target_var: 1Darray.target variable
-        features: NDarray.feature values.
-    return value of cost function
+        theta: List of 1Darrays.Parametes of regression.
+        target_var: 1Darray.Target variable.
+        features: 2Darray.Feature values.
+    return values of cost function
     """
-    # サンプルデータ数を定義
-    m = len(target_var)
-    # target_varをm行1列にreshape
-    target_var_matrix = target_var.reshape(m, 1)    
+    # 目的変数の数と特徴量1種類あたりの数が等しくなければエラーメッセージを返す。
+    if len(target_var) != len(features):
+        print("Error: Invalid args")
+        exit()
+        
+    # パラメータの全組み合わせをリスト化し、配列に変換
+    theta_array = np.array(list(product(*theta)))
     
     # featuresが1次元配列であれば、1行m列の2次元配列にする
     # そうでなければ転置する
+    m = len(target_var)
     if features.ndim == 1:
         features = features.reshape(1, m)
     else:
@@ -23,11 +33,11 @@ def cost_func(theta, target_var, features):
     # 1行m列の単位行列を定義
     ones  = np.ones((1, m))
     # 特徴量の配列の1行目にonesを結合
-    ones_features = np.concatenate([ones,features])
+    features_array = np.concatenate([ones,features])
     
-    # 残差の二乗の計算
-    sq_errors = (target_var_matrix - np.dot(ones_features.T, theta.T)) ** 2
+    # 残差の二乗の計算。線形回帰モデルの式を内積で表している。
+    sq_errors = (target_var - np.dot(theta_array, features_array)) ** 2
     # mseを返す
-    return np.mean(sq_errors)
+    return sq_errors.mean(axis=1)
     
     
