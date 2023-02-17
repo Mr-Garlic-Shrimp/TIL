aecdec9d28b1af63d7569b267e534f59a0264996
Mr-Garlic-Shrimp <pometango50.19@gmail.com>
2023-02-17 23:35:39 +0900

    Polarsをインストール

--- Python/pandas/learning-dataframe.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df_sales = pd.read_csv('Sales-sample.csv', index_col=['Sales_No'])
-# dirを指定
-#df_sales = pd.read_csv(r'C:\Users\1stel\Downloads\T_Sales_Header (1).csv', index_col=['Sales_No'])
-print(df_sales.iloc[3:6, 0:2])
-
